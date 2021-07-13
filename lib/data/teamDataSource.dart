5d0bfa7e815418201ed69da1079022ef5251f00c
black-redoc <joseb.twelve@gmail.com>
2021-07-13 09:01:38 -0500

    added teamDataSource in data layer

---
+++ lib/data/teamDataSource.dart
@@ -0,0 +1,37 @@
+import 'package:biblioteca_esfemica/domain/Team.dart';
+
+class TeamDataSource {
+  
+  List<Team> getTeamList() {
+    return [
+      Team(
+        "Amanda",
+        "Jones",
+        "Directora General",
+        "Abogada",
+        "assets/director.jpg",
+      ),
+      Team(
+        "Lidia",
+        "Soles",
+        "Directora Comunicaciones",
+        "Psicologa",
+        "assets/operations.jpg",
+      ),
+      Team(
+        "Melida",
+        "Giraldo",
+        "Directora Redacción",
+        "Comunicadora Social",
+        "assets/comunicator.jpg",
+      ),
+      Team(
+        "Alicia",
+        "Grajales",
+        "Directora Administrativa",
+        "Economista",
+        "assets/admion.jpg",
+      ),
+    ];
+  }
+}
--- lib/domain/Team.dart
+++ lib/domain/Team.dart
@@ -3,16 +3,18 @@ class Team {
   final String? _lastName;
   final String? _workArea;
   final String? _carrer;
+  final String? _imageUrl;
 
   Team(
     this._firstName,
     this._lastName,
     this._workArea,
-    this._carrer
+    this._carrer,
+    this._imageUrl
   );
 
   String get completeName => "$_firstName $_lastName";
   String get workArea => "$_workArea";
   String get carrer => "$_carrer";
-
+  String get imageUrl => "$_imageUrl";
 }
