64564c69a4290dafafc8efa0f1d95d927a5e121d
black-redoc <joseb.twelve@gmail.com>
2021-07-15 11:22:24 -0500

    created Book and BookCarrousel models in domain layer

---
+++ lib/domain/Book.dart
@@ -0,0 +1,11 @@
+class Book {
+  final String? name;
+  final String? author;
+  final DateTime? releaseDate;
+
+  Book({
+    this.name,
+    this.author,
+    this.releaseDate
+  });
+}
---
+++ lib/domain/BookCarrousel.dart
@@ -0,0 +1,12 @@
+import 'package:biblioteca_esfemica/domain/Book.dart';
+
+
+class BookCarrousel {
+  final String? genre;
+  final List<Book>? books;
+
+  BookCarrousel({
+    this.genre,
+    this.books
+  });
+}
