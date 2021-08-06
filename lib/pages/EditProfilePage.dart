017bcfa18a95e6905a3f473001d274effa0d4904
black-redoc <joseb.twelve@gmail.com>
2021-08-06 14:22:47 -0500

    added interest list view with dismissible

--- lib/pages/EditProfilePage.dart
+++ lib/pages/EditProfilePage.dart
@@ -1,13 +1,42 @@
+import 'package:biblioteca_esfemica/widgets/buttons/roundedRedButton.dart';
 import 'package:biblioteca_esfemica/widgets/texts/mediumText.dart';
+import 'package:biblioteca_esfemica/widgets/texts/smallText.dart';
 import 'package:biblioteca_esfemica/widgets/titlebar/redArcHeader.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class EditProfilePage extends StatelessWidget {
+class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
 
   @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  List<String>? interestList;
+
+  @override
+  void initState() {
+    super.initState();
+    interestList = [
+      "Drama",
+      "Feminismo",
+      "Sociología",
+      "Ciencias Sociales",
+      "Drama",
+      "Feminismo",
+      "Sociología",
+      "Ciencias Sociales",
+      "Drama",
+      "Feminismo",
+      "Sociología",
+      "Ciencias Sociales"
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -32,13 +61,147 @@ class EditProfilePage extends StatelessWidget {
           SizedBox(height: 40),
           MediumText(
             text: "Intereses",
+          ),
+          SizedBox(height: 14),
+          Expanded(
+            child: _interestsList(size),
           )
         ]
       ),
     );
   }
 
+  void _deleteInterest(int id) {
+    setState(() {
+      interestList?.removeAt(id);
+      Navigator.of(context).pop();
+    });
+  }
+
+  Future<bool> _confirmDismiss(int id) async {
+    final response = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              SizedBox(width: 20),
+              SmallText(text: "Confirmación"),
+              IconButton(
+                onPressed: () => _backPage(context),
+                icon: FaIcon(FontAwesomeIcons.times, size: 20)
+              )
+            ],
+          ),
+          content: SmallText(
+            text: "Desea eliminar este item?"
+          ),
+          actions: <Widget>[
+            RoundedRedButton(
+              child: SmallText(
+                text: "Eliminar",
+                color: Colors.white
+              ),
+              onPressed: () => _deleteInterest(id)
+            ),
+          ],
+        );
+      }
+    );
+    return response;
+  }
+
   void _backPage(BuildContext context) => Navigator.of(context).pop();
+
+  Widget _interestsList(Size size) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+      itemCount: interestList?.length,
+      itemBuilder: (BuildContext context, int idx) {
+        return Container(
+          width: size.width,
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(color: Colors.grey, width: 0.5),
+              bottom: (idx == (interestList!.length - 1))
+              ? BorderSide(color: Colors.grey, width: 0.5)
+              : BorderSide.none,
+            )
+          ),
+          child: Dismissible(
+            key: Key("${interestList?[idx]}"),
+            confirmDismiss: (DismissDirection direction) => _confirmDismiss(idx),
+            child: ListTile(
+              title: Center(
+                child: SmallText(
+                  text: "${interestList?[idx]}",
+                  color: Colors.red
+                ),
+              ),
+            ),
+            background: _slideRightBackground(),
+            secondaryBackground: _slideLeftBackground(),
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _slideRightBackground() => Container(
+    color: Colors.red,
+    child: Align(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            width: 20,
+          ),
+          FaIcon(
+            FontAwesomeIcons.trash,
+            color: Colors.white,
+          ),
+          Text(
+            " Eliminar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ],
+      ),
+      alignment: Alignment.centerLeft,
+    ),
+  );
+
+  Widget _slideLeftBackground() => Container(
+    color: Colors.red,
+    child: Align(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Text(
+            " Eliminar",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          FaIcon(
+            FontAwesomeIcons.trash,
+            color: Colors.white,
+          ),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      alignment: Alignment.centerRight,
+    ),
+  );
 }
 
 
